d8f68fce5db001ff31e18a515c9aae6334fae149
Mokhtar-dev <generalmk122@gmail.com>
2026-03-15 13:23:11 -0700

    i added all the tasks that we did in the section

---
+++ Tasks_Requierd/Task1_Section1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2, num3;
+
+    printf("Enter the first number:\n");
+    scanf("%d", &num1);
+
+    printf("Enter the second number:\n");
+    scanf("%d", &num2);
+
+    printf("Enter the third number:\n");
+    scanf("%d", &num3);
+
+    int sum = num1 + num2 + num3;
+    printf("The sum of the three numbers is: %d\n", sum);
+    
+    return 0;
+}
